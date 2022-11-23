cd4e1865492fa0c7d1177804d3fa9dfee997d461
SakenT <SakenT@001-11>
2022-11-23 19:09:16 +0600

    Добавьте файлы проекта.

---
+++ 1st.StepProject/1st.projectStep.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+	float a;
+	float b;
+	float S2;
+
+
+   setlocale(LC_ALL, "RUS");
+	cout << "¬ведите данные вам стороны: \n";
+	cout << "a = ";
+	cin >> a;
+
+	cout << "b = ";
+	cin >> b; 
+	S2 = (a + b) * 2;
+	cout << S2;
+
+
+}
